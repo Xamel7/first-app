cb65192bd6fc1dc6112d1686131e34ab417b951e
Xamel7 <123972550+Xamel7@users.noreply.github.com>
2023-04-25 14:48:58 -0500

    Changes

--- src/HornedBeasts.js
+++ src/HornedBeasts.js
@@ -3,27 +3,36 @@
 // import Header from './Header.js';
 // import Footer from './Footer.js';
 import {useState} from 'react';
+import Card from "react-bootstrap/Card";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function HornedBeasts(props) {
   let [isFavorited, setIsFavorited] = useState(false)
-  let [defaultDisplay, setDefaultDisplay] = useState("black")
- function onClick(){
+  let [defaultDisplay, setDefaultDisplay] = useState('black')
+  let [timesClicked, setTimesClicked] = useState(0)
+ function handleClick(){
   setIsFavorited(!isFavorited);
   //if statement to determine what to setDefaultDisplay to.
   // console.log(isFavorited)
   if(!isFavorited){
     setDefaultDisplay('black')
+    setTimesClicked(timesClicked + 1);
     }else {
       setDefaultDisplay('red')
   }
+  console.log('fave state ', isFavorited)
+  console.log('display state ', defaultDisplay)
  };
   return (
-    <div>
-    <h2>{props.title}</h2>
-    <img src={props.image} alt="beast" width={300}></img>
-    <p>{props.description}</p>
-    <p onClick={onClick} style={{color: defaultDisplay}}>&#10084;</p>
-    </div>
+    <Card style={{maxWidth:"30%", textAlign:"center", display:"inline-grid", objectFit:'contain', borderStyle:"solid"}}>
+      <Card.Body>
+      <Card.Title>{props.title}</Card.Title>
+      <Card.Img src={props.image} alt="beast" width={100}></Card.Img>
+      <Card.Text>{props.description}</Card.Text>
+      <Card.Text onClick={handleClick} className="text-dark" style = {{color:'black' }}>{ isFavorited===true ? "❤️" : "🖤"}{timesClicked}</Card.Text>
+      </Card.Body>
+    </Card>
     );
 }
 
--- src/Main.js
+++ src/Main.js
@@ -4,25 +4,18 @@
 // import Footer from './Footer.js';
 import HornedBeasts from './HornedBeasts';
 import data from './data.json';
-import Card from "react-bootstrap/Card";
-import Button from "react-bootstrap/Button";
+// import './Main.CSS'
+// import Button from "react-bootstrap/Button";
+
 
 function Main() {
   return (
     
-    <div>
-      <Card>
-        <Card.Img variant="primary" />
-        <Card.Body>
-          <Card.Title>Deck Of Cards</Card.Title>
-          <Card.Text>
-            An inventory of the images I've collected thus far.
-          </Card.Text>
-          <Button variant="primary">Click</Button>
-        </Card.Body>
-      </Card>
+    <div className='container' style={{width: "100%", alignItems: "center"}}>
+      {/*  */}
+      {/* Looping through the data.JSON to render images on my page. */}
       {data.map(function(value,index){
-        return <HornedBeasts title={data[index].title}image={data[index].image_url}description={data[index].description}/>
+        return <HornedBeasts key={value._id} beast={value} title={data[index].title}image={data[index].image_url}description={data[index].description}/>
       })};
     </div>
     );
